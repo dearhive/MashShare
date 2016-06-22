f008d43afcb179f47d5f22513bfe3261739797d8
René Hermenau <admin@xsimulator.net>
2016-06-22 11:45:48 +0200

    small fix in admin footer

--- includes/admin/admin-footer.php
+++ includes/admin/admin-footer.php
@@ -15,14 +15,11 @@ if( !defined( 'ABSPATH' ) ) exit;
 /**
  * Add rating links to the settings footer
  *
- * @since	    1.0.0
- * @global		string $typenow
+ * @since	1.0.0
  * @param       string $footer_text The existing footer text
  * @return      string
  */
-function mashsb_admin_rate_us( $footer_text ) {
-	global $typenow;
-
+function mashsb_admin_rate_us() {
 	if ( mashsb_is_admin_page() ) {
 		$rate_text = sprintf( __( 'Thank you for using <a href="%1$s" target="_blank">Mashshare</a>! Please <a href="%2$s" target="_blank">rate us</a> on <a href="%2$s" target="_blank">WordPress.org</a> and help to support this project.<br>Something not working as expected or need help? Read our <a href="%3$s" target="blank">Documentation</a>', 'mashsb' ),
 			'https://www.mashshare.net',
@@ -30,9 +27,6 @@ function mashsb_admin_rate_us( $footer_text ) {
 			'https://www.mashshare.net/documentation/'
 		);
 
-		return $rate_text . '</span>';
-	} else {
-		return $footer_text;
+		return $rate_text;
 	}
 }
-//add_filter( 'admin_footer_text', 'mashsb_admin_rate_us' );
