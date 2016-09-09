c29e26c3d18d1737bf6cb7b4a977c33b3d5b295c
René Hermenau <admin@xsimulator.net>
2016-09-09 15:52:24 +0200

    test only php 7

--- tests/test-template-functions.php
+++ tests/test-template-functions.php
@@ -26,7 +26,8 @@ class TemplateFunctions extends WP_UnitTestCase {
             require_once(MASHSB_PLUGIN_DIR . 'includes/mashengine.php');
         $mashsbSharesObj = new mashengine($url);
         $shares = $mashsbSharesObj->getFBTWCounts()->total;
-
+        $this->expectOutputString('foo');
+        var_dump($shares);
         fwrite(STDERR, 'TEST: ' . print_r($shares, TRUE));
         $this->assertGreaterThan(0, $shares);
     }
