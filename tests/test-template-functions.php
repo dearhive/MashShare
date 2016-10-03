c85cd944edf2bb7cb3f469035ba69f0cc2597ec6
René Hermenau <admin@xsimulator.net>
2016-10-03 14:58:14 +0200

    higher pphunit test

--- tests/test-template-functions.php
+++ tests/test-template-functions.php
@@ -64,7 +64,9 @@ class TemplateFunctions extends WP_UnitTestCase {
         $url = 'http://google.com';
         
         $mashsbSharesObj = new mashengine($url);
-        $shares = $mashsbSharesObj->{getALLCounts()}->total;
+        
+        $shares1 = $mashsbSharesObj->getALLCounts();
+        $shares = $shares1->total;
 
         $this->assertGreaterThan(1000, (int)$shares);
     }
@@ -76,7 +78,7 @@ class TemplateFunctions extends WP_UnitTestCase {
         $url = 'http://google.com';
         
         $mash = new mashengine($url);
-        $shares = $mash->{getALLCounts()}->total;
+        $shares = $mash->getALLCounts()->total;
         
         $this->assertGreaterThan(1000, (int)$shares);
     }
