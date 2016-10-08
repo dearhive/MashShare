7a4d19e7b9863e997421e87411ae999ec16c8ba5
René Hermenau <info@mashshare.net>
2016-10-08 12:16:32 +0200

    change tests

--- tests/test-template-functions.php
+++ tests/test-template-functions.php
@@ -12,7 +12,7 @@ class TemplateFunctions extends WP_UnitTestCase {
         $mashsb_options['disable_cache'] = 'true';
         //$mashsb_options['fb_access_token'] = 'EAAHag2FMn2UBAJwIjdG2jDB39ZAr3lCriDwXgqhk2hDxk1O1iM5Vk2WvxIXop6ZAUikeKcpBXWUQk2jxG8FXag4cEzXJDBggkWIGuEq9ECc6HAyG1UQrgaDr6w8M0tsT6tlHwBpjVGACyQectEU3CdFAgbX32Q83qGZAgv4cbWH39eb3ejc';
         // enable permalinks
-        update_option( 'permalink_structure', '/%postname%/' );
+        update_option('permalink_structure', '/%postname%/');
 
         require_once MASHSB_PLUGIN_DIR . 'includes/libraries/RolingCurlX.php';
         require_once(MASHSB_PLUGIN_DIR . 'includes/mashengine.php');
@@ -28,32 +28,32 @@ class TemplateFunctions extends WP_UnitTestCase {
     public function test_mashsb_is_cache_refresh() {
         global $mashsb_options;
         $mashsb_options['disable_cache'] = 'true';
-        $this->assertTrue( mashsb_is_cache_refresh() );
+        $this->assertTrue(mashsb_is_cache_refresh());
     }
 
     public function test_mashsb_force_cache_refresh() {
         global $mashsb_options;
         $mashsb_options['disable_cache'] = 'true';
-        $this->assertTrue( mashsb_force_cache_refresh() );
+        $this->assertTrue(mashsb_force_cache_refresh());
     }
 
     public function test_mashsb_is_enabled_permalinks() {
-        $this->assertTrue( mashsb_is_enabled_permalinks() );
+        $this->assertTrue(mashsb_is_enabled_permalinks());
     }
 
     public function test_is_singular() {
         global $post;
         // Prepare test
-        $post_id = $this->factory->post->create( array(
+        $post_id = $this->factory->post->create(array(
             'post_title' => 'Hello World',
             'post_name' => 'hello-world',
             'post_type' => 'post',
             'post_status' => 'publish'
-                ) );
-        $this->go_to( get_permalink( $post_id ) );
-        $post = get_post( $post_id );
+                ));
+        $this->go_to(get_permalink($post_id));
+        $post = get_post($post_id);
 
-        $this->assertTrue( is_singular() );
+        $this->assertTrue(is_singular());
     }
 
     public function test_mashengine_FBTW() {
@@ -61,16 +61,19 @@ class TemplateFunctions extends WP_UnitTestCase {
         delete_transient('mashsb_rate_limit');
         delete_transient('mashsb_limit_req');
 
+        sleep(2);
+
         $url = 'http://google.com';
 
-        $mashsbSharesObj = new mashengine( $url );
+        $mashsbSharesObj = new mashengine($url);
 
         $shares = $mashsbSharesObj->getALLCounts()->total;
 
-        $this->assertGreaterThan( 1000, ( int ) $shares );
+        $this->assertGreaterThan(1000, (int) $shares);
     }
 
     public function test_mashengine_all_counts() {
+        sleep(2);
         //delete transients
         delete_transient('mashsb_rate_limit');
         delete_transient('mashsb_limit_req');
@@ -79,13 +82,14 @@ class TemplateFunctions extends WP_UnitTestCase {
         //$mashsb_options['fb_access_token'] = 'EAAHag2FMn2UBAJwIjdG2jDB39ZAr3lCriDwXgqhk2hDxk1O1iM5Vk2WvxIXop6ZAUikeKcpBXWUQk2jxG8FXag4cEzXJDBggkWIGuEq9ECc6HAyG1UQrgaDr6w8M0tsT6tlHwBpjVGACyQectEU3CdFAgbX32Q83qGZAgv4cbWH39eb3ejc';
         $url = 'http://google.com';
 
-        $mash = new mashengine( $url );
+        $mash = new mashengine($url);
         $shares = $mash->getALLCounts()->total;
 
-        $this->assertGreaterThan( 1000, ( int ) $shares );
+        $this->assertGreaterThan(1000, (int) $shares);
     }
 
     public function test_getSharedcount() {
+        sleep(2);
         global $mashsb_options, $post;
         //delete transients
         delete_transient('mashsb_rate_limit');
@@ -97,21 +101,22 @@ class TemplateFunctions extends WP_UnitTestCase {
         $mashsb_options['disable_cache'] = 'true';
 
         $args = array('post_type' => 'post');
-        $id = $this->factory->post->create( $args );
-        $this->go_to( get_permalink( $id ) );
-        $post = get_post( $id ); // We need the post object for testing
+        $id = $this->factory->post->create($args);
+        $this->go_to(get_permalink($id));
+        $post = get_post($id); // We need the post object for testing
 
         $url = 'http://google.com';
         $url2 = 'https://google.com';
 
-        $shares = getSharedcount( $url );
-        $shares2 = getSharedcount( $url2 );
+        $shares = getSharedcount($url);
+        $shares2 = getSharedcount($url2);
 
-        $this->assertGreaterThan( 1000, ( int ) $shares );
-        $this->assertGreaterThan( 1000, ( int ) $shares2 );
+        $this->assertGreaterThan(1000, (int) $shares);
+        $this->assertGreaterThan(1000, (int) $shares2);
     }
 
     public function test_getSharedcountJson() {
+        sleep(2);
         global $mashsb_options, $post;
         //delete transients
         delete_transient('mashsb_rate_limit');
@@ -122,28 +127,29 @@ class TemplateFunctions extends WP_UnitTestCase {
         $mashsb_options['disable_cache'] = 'true';
 
         $args = array('post_type' => 'post');
-        $id = $this->factory->post->create( $args );
-        $this->go_to( get_permalink( $id ) );
-        $post = get_post( $id ); // We need the post object for testing
+        $id = $this->factory->post->create($args);
+        $this->go_to(get_permalink($id));
+        $post = get_post($id); // We need the post object for testing
 
         $url = 'http://google.com';
 
         // Get the share Object
-        $mashsbSharesObj = mashsbGetShareObj( $url );
-        
+        $mashsbSharesObj = mashsbGetShareObj($url);
+
         // Get the share count Method
-        $mashsbShareCounts = mashsbGetShareMethod( $mashsbSharesObj );
+        $mashsbShareCounts = mashsbGetShareMethod($mashsbSharesObj);
         $encode_data = json_encode($mashsbShareCounts);
-        
+
         $decode_data = json_decode($encode_data, true);
 
         $this->assertArrayHasKey('facebook_shares', $decode_data);
-          
+
         $facebook_shares = $mashsbShareCounts->facebook_shares;
-        $this->assertGreaterThan( 1000, $facebook_shares );
+        $this->assertGreaterThan(1000, $facebook_shares);
     }
 
     public function test_getSharedcount_async_cache() {
+        sleep(2);
         global $mashsb_options, $post;
         //delete transients
         delete_transient('mashsb_rate_limit');
@@ -155,39 +161,39 @@ class TemplateFunctions extends WP_UnitTestCase {
         $mashsb_options['disable_cache'] = 'true';
 
         $args = array('post_type' => 'post');
-        $id = $this->factory->post->create( $args );
-        $this->go_to( get_permalink( $id ) );
-        $post = get_post( $id ); // Wee need the post object for testing
+        $id = $this->factory->post->create($args);
+        $this->go_to(get_permalink($id));
+        $post = get_post($id); // Wee need the post object for testing
 
 
         $url = 'http://google.com';
         $url2 = 'https://google.com';
 
-        $shares = getSharedcount( $url );
-        $shares2 = getSharedcount( $url2 );
+        $shares = getSharedcount($url);
+        $shares2 = getSharedcount($url2);
 
-        $this->assertGreaterThan( 1000, ( int ) $shares );
-        $this->assertGreaterThan( 1000, ( int ) $shares2 );
+        $this->assertGreaterThan(1000, (int) $shares);
+        $this->assertGreaterThan(1000, (int) $shares2);
     }
 
     public function test_is_active_on_page() {
         global $post, $mashsb_options;
 
         // Prepare test
-        $post_id = $this->factory->post->create( array(
+        $post_id = $this->factory->post->create(array(
             'post_title' => 'Hello World',
             'post_name' => 'hello-world',
             'post_type' => 'page',
             'post_status' => 'publish'
-                ) );
-        $this->go_to( get_permalink( $post_id ) );
+                ));
+        $this->go_to(get_permalink($post_id));
         //$id = $this->factory->post->create($args);
         $mashsb_options['post_types'] = array('page');
-        $this->go_to( get_permalink( $post_id ) );
-        $post = get_post( $post_id ); // Wee need the post object for testing
+        $this->go_to(get_permalink($post_id));
+        $post = get_post($post_id); // Wee need the post object for testing
 
 
-        $this->assertTrue( mashsbGetActiveStatus() );
+        $this->assertTrue(mashsbGetActiveStatus());
     }
 
     public function test_is_active_on_post() {
@@ -195,15 +201,15 @@ class TemplateFunctions extends WP_UnitTestCase {
         $mashsb_options['post_types'] = array('post');
 
         // Prepare test
-        $post_id = $this->factory->post->create( array(
+        $post_id = $this->factory->post->create(array(
             'post_type' => 'post'
-                ) );
+                ));
         //$id = $this->factory->post->create($args);
 
-        $this->go_to( get_permalink( $post_id ) );
-        $post = get_post( $post_id ); // Wee need the post object for testing
+        $this->go_to(get_permalink($post_id));
+        $post = get_post($post_id); // Wee need the post object for testing
 
-        $this->assertTrue( mashsbGetActiveStatus() );
+        $this->assertTrue(mashsbGetActiveStatus());
     }
 
 //    public function test_is_active_on_frontpage() {
@@ -232,25 +238,25 @@ class TemplateFunctions extends WP_UnitTestCase {
         global $mashsb_options;
         $mashsb_options['frontpage'] = 'true';
 
-        $page_on_front = $this->factory->post->create( array(
+        $page_on_front = $this->factory->post->create(array(
             'post_type' => 'page',
-                ) );
-        $page_for_posts = $this->factory->post->create( array(
+                ));
+        $page_for_posts = $this->factory->post->create(array(
             'post_type' => 'page',
-                ) );
-        update_option( 'show_on_front', 'page' );
-        update_option( 'page_on_front', $page_on_front );
-        update_option( 'page_for_posts', $page_for_posts );
-        $this->go_to( '/' );
-        $this->assertQueryTrue( 'is_front_page', 'is_page', 'is_singular' );
-        $this->assertTrue( mashsbGetActiveStatus() );
+                ));
+        update_option('show_on_front', 'page');
+        update_option('page_on_front', $page_on_front);
+        update_option('page_for_posts', $page_for_posts);
+        $this->go_to('/');
+        $this->assertQueryTrue('is_front_page', 'is_page', 'is_singular');
+        $this->assertTrue(mashsbGetActiveStatus());
 
-        $this->go_to( get_permalink( $page_for_posts ) );
-        $this->assertQueryTrue( 'is_home', 'is_posts_page' );
+        $this->go_to(get_permalink($page_for_posts));
+        $this->assertQueryTrue('is_home', 'is_posts_page');
 
-        update_option( 'show_on_front', 'posts' );
-        delete_option( 'page_on_front' );
-        delete_option( 'page_for_posts' );
+        update_option('show_on_front', 'posts');
+        delete_option('page_on_front');
+        delete_option('page_for_posts');
     }
 
     public function test_is_active_on_content_shortcode() {
@@ -264,11 +270,11 @@ class TemplateFunctions extends WP_UnitTestCase {
             'post_type' => 'page',
             'post_content' => '[mashshare]'
         );
-        $id = $this->factory->post->create( $args );
-        $this->go_to( get_permalink( $id ) );
-        $post = get_post( $id ); // Wee need the post object for testing
+        $id = $this->factory->post->create($args);
+        $this->go_to(get_permalink($id));
+        $post = get_post($id); // Wee need the post object for testing
 
-        $this->assertTrue( mashsbGetActiveStatus() );
+        $this->assertTrue(mashsbGetActiveStatus());
     }
 
     public function test_get_fakecount_for_english() {
@@ -280,10 +286,10 @@ class TemplateFunctions extends WP_UnitTestCase {
             'post_status' => 'publish',
             'post_type' => 'page',
         );
-        $id = $this->factory->post->create( $args );
-        $post = get_post( $id ); // Wee need the post object for testing
+        $id = $this->factory->post->create($args);
+        $post = get_post($id); // Wee need the post object for testing
 
-        $this->assertEquals( 0.30, mashsb_get_fake_factor() );
+        $this->assertEquals(0.30, mashsb_get_fake_factor());
     }
 
     public function test_get_fakecount_for_hebraic() {
@@ -295,10 +301,10 @@ class TemplateFunctions extends WP_UnitTestCase {
             'post_status' => 'publish',
             'post_type' => 'page',
         );
-        $id = $this->factory->post->create( $args );
-        $post = get_post( $id ); // Wee need the post object for testing
+        $id = $this->factory->post->create($args);
+        $post = get_post($id); // Wee need the post object for testing
 
-        $this->assertEquals( 0.60, mashsb_get_fake_factor() );
+        $this->assertEquals(0.60, mashsb_get_fake_factor());
     }
 
     public function test_mashsb_hide_shares() {
@@ -306,15 +312,15 @@ class TemplateFunctions extends WP_UnitTestCase {
         $mashsb_options['hide_sharecount'] = 60;
 
         $shares = '';
-        $this->assertTrue( mashsb_hide_shares( $shares ) );
+        $this->assertTrue(mashsb_hide_shares($shares));
         $shares = 0;
-        $this->assertTrue( mashsb_hide_shares( $shares ) );
+        $this->assertTrue(mashsb_hide_shares($shares));
         $shares = 0.1;
-        $this->assertTrue( mashsb_hide_shares( $shares ) );
+        $this->assertTrue(mashsb_hide_shares($shares));
         $shares = 60;
-        $this->assertFalse( mashsb_hide_shares( $shares ) ); // Must be false        
+        $this->assertFalse(mashsb_hide_shares($shares)); // Must be false        
         $shares = 65;
-        $this->assertFalse( mashsb_hide_shares( $shares ) ); // Must be false
+        $this->assertFalse(mashsb_hide_shares($shares)); // Must be false
     }
 
 }
