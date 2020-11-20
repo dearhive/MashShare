22c94dbdcca1ee47f99e61c8dafdddda2246267d
Federico Jacobi <fjacobi@bhmdigital.com>
2020-11-20 17:09:09 -0500

    jQuery nearest must run before the .ready callback

--- assets/js/mashsb.js
+++ assets/js/mashsb.js
@@ -1,5 +1,53 @@
 var strict;
 
+/*!------------------------------------------------------
+ * jQuery nearest v1.0.3
+ * http://github.com/jjenzz/jQuery.nearest
+ * ------------------------------------------------------
+ * Copyright (c) 2012 J. Smith (@jjenzz)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ */
+(function ($, d) {
+    $.fn.nearest = function (selector) {
+        var self, nearest, el, s, p,
+                hasQsa = d.querySelectorAll;
+
+        function update(el) {
+            nearest = nearest ? nearest.add(el) : $(el);
+        }
+
+        this.each(function () {
+            self = this;
+
+            $.each(selector.split(','), function () {
+                s = $.trim(this);
+
+                if (!s.indexOf('#')) {
+                    // selector starts with an ID
+                    update((hasQsa ? d.querySelectorAll(s) : $(s)));
+                } else {
+                    // is a class or tag selector
+                    // so need to traverse
+                    p = self.parentNode;
+                    while (p) {
+                        el = hasQsa ? p.querySelectorAll(s) : $(p).find(s);
+                        if (el.length) {
+                            update(el);
+                            break;
+                        }
+                        p = p.parentNode;
+                    }
+                }
+            });
+
+        });
+
+        return nearest || $();
+    };
+}(jQuery, document));
+
 jQuery(document).ready(function ($) {
 
     /* Show Whatsapp button on mobile devices iPhones and Android only */
@@ -437,52 +485,3 @@ jQuery(document).ready(function ($) {
         $('.mashsbcount').countTo({from: 0, to: mashsb.shares, speed: 1000, refreshInterval: 100});
     }
 });
-
-
-/*!------------------------------------------------------
- * jQuery nearest v1.0.3
- * http://github.com/jjenzz/jQuery.nearest
- * ------------------------------------------------------
- * Copyright (c) 2012 J. Smith (@jjenzz)
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- */
-(function ($, d) {
-    $.fn.nearest = function (selector) {
-        var self, nearest, el, s, p,
-                hasQsa = d.querySelectorAll;
-
-        function update(el) {
-            nearest = nearest ? nearest.add(el) : $(el);
-        }
-
-        this.each(function () {
-            self = this;
-
-            $.each(selector.split(','), function () {
-                s = $.trim(this);
-
-                if (!s.indexOf('#')) {
-                    // selector starts with an ID
-                    update((hasQsa ? d.querySelectorAll(s) : $(s)));
-                } else {
-                    // is a class or tag selector
-                    // so need to traverse
-                    p = self.parentNode;
-                    while (p) {
-                        el = hasQsa ? p.querySelectorAll(s) : $(p).find(s);
-                        if (el.length) {
-                            update(el);
-                            break;
-                        }
-                        p = p.parentNode;
-                    }
-                }
-            });
-
-        });
-
-        return nearest || $();
-    };
-}(jQuery, document));
